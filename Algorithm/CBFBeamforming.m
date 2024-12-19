f8c77d22e031ed2f75a287e35f8b2ed41938e1e6
TJU_cc180 <99528943+140598cc180@users.noreply.github.com>
2024-12-19 16:40:16 +0800

    常规波束形成(CBF)
     常规波束形成(CBF)

--- Algorithm/CBFBeamforming.m
+++ Algorithm/CBFBeamforming.m
@@ -17,24 +17,18 @@ function [y_cbf_db, theta] = CBFBeamforming(received_signal, frequency, sound_sp
     % 输出参数:
     %   y_cbf_db - 波束形成后的分贝值输出
     %   theta - 对应的角度向量
-    
     % 生成角度向量
     theta = angle_start:d_theta:angle_end;
     sine_theta = sind(theta);
-    
     % 生成阵元序号向量
     n = 0:(sensor_number-1);
-    
     % 计算波数
     k = 2 * pi * frequency / sound_speed;
-    
     % 计算导向矢量
     steering_vector = (1 / sensor_number) * ...
         exp(-1i * k * n.' * sensor_distance * sine_theta);
-    
     % 波束形成
     y_cbf = steering_vector' * received_signal;
-    
     % 转换为分贝值
     y_cbf_db = trans2db(abs(y_cbf).^2, db_floor);
 end
